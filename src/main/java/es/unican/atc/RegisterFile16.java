5a0f88334305d0060d85279098439e2a6d852bd1
Esteban Stafford <esteban.stafford@unican.es>
2023-10-23 19:16:21 +0200

    Paint register contents

--- src/main/java/es/unican/atc/RegisterFile16.java
+++ src/main/java/es/unican/atc/RegisterFile16.java
@@ -21,30 +21,44 @@ import com.cburch.logisim.util.StringUtil;
 class RegisterFile16 extends InstanceFactory {
 
    private static final Attribute[] ATTRIBUTES = { StdAttr.TRIGGER };
+   public static final int A1 = 0;
+   public static final int A2 = 1;
+   public static final int A3 = 2;
+   public static final int WD3 = 3;
+   public static final int R15 = 4;
+
+   public static final int CLK = 5;
+   public static final int CLR = 6;
+   public static final int WE3 = 7;
+
+   public static final int RD1 = 8;
+   public static final int RD2 = 9;
+
+   public static final int NUM_REGISTERS = 16;
+   public static final int REGISTER_WIDTH = 32;
+
+   public static final int DEVICE_WIDTH = 140;
+   public static final int DEVICE_HEIGHT = 200;
 
    RegisterFile16() {
       super("RegisterFile16", new SimpleStringGetter("16x32 Register File"));
-      int width = 32;
-      int length = 16;
-      int address_width = (int)(Math.log(length)/Math.log(2));
-      int device_width = 100;
-      int device_height = 160;
+      int address_width = (int)(Math.log(NUM_REGISTERS)/Math.log(2));
       setAttributes(new Attribute[] { StdAttr.TRIGGER }, new AttributeOption[] { StdAttr.TRIG_RISING });
-      setOffsetBounds(Bounds.create(-device_width/2, -device_height/2, device_width, device_height));
-      setPorts(new Port []{
-         new Port(-device_width/2, -device_height/2 +20, Port.INPUT, address_width),
-         new Port(-device_width/2, -device_height/2 +40, Port.INPUT, address_width),
-         new Port(-device_width/2, -device_height/2 +60, Port.INPUT, address_width),
-         new Port(-device_width/2, -device_height/2 +80, Port.INPUT, width),
-         new Port(-device_width/2, -device_height/2 +100, Port.INPUT, width),
+      setOffsetBounds(Bounds.create(-DEVICE_WIDTH/2, -DEVICE_HEIGHT/2, DEVICE_WIDTH, DEVICE_HEIGHT));
+      Port[] ports = new Port[10];
+      ports[A1] = new Port(-DEVICE_WIDTH/2, -DEVICE_HEIGHT/2 +20, Port.INPUT, address_width);
+      ports[A2] = new Port(-DEVICE_WIDTH/2, -DEVICE_HEIGHT/2 +40, Port.INPUT, address_width);
+      ports[A3] = new Port(-DEVICE_WIDTH/2, -DEVICE_HEIGHT/2 +60, Port.INPUT, address_width);
+      ports[WD3] = new Port(-DEVICE_WIDTH/2, -DEVICE_HEIGHT/2 +80, Port.INPUT, REGISTER_WIDTH);
+      ports[R15] = new Port(-DEVICE_WIDTH/2, -DEVICE_HEIGHT/2 +100, Port.INPUT, REGISTER_WIDTH);
 
-         new Port(-device_width/2 +50, device_height/2, Port.INPUT, 1),
-         new Port(-device_width/2 +30, -device_height/2, Port.INPUT, 1),
-         new Port(-device_width/2 +60, -device_height/2, Port.INPUT, 1),
+      ports[CLK] = new Port(-DEVICE_WIDTH/2 +50, DEVICE_HEIGHT/2, Port.INPUT, 1);
+      ports[CLR] = new Port(-DEVICE_WIDTH/2 +30, -DEVICE_HEIGHT/2, Port.INPUT, 1);
+      ports[WE3] = new Port(-DEVICE_WIDTH/2 +60, -DEVICE_HEIGHT/2, Port.INPUT, 1);
 
-         new Port(device_width/2, -device_height/2 +20, Port.OUTPUT, width),
-         new Port(device_width/2, -device_height/2 +40, Port.OUTPUT, width),
-      });
+      ports[RD1] = new Port(DEVICE_WIDTH/2, -DEVICE_HEIGHT/2 +20, Port.OUTPUT, REGISTER_WIDTH);
+      ports[RD2] = new Port(DEVICE_WIDTH/2, -DEVICE_HEIGHT/2 +40, Port.OUTPUT, REGISTER_WIDTH);
+      setPorts(ports);
 
       //setInstancePoker(RegisterPoker.class);
    }
@@ -55,12 +69,12 @@ class RegisterFile16 extends InstanceFactory {
       AttributeOption triggerType = state.getAttributeValue(StdAttr.TRIGGER);
       BitWidth WIDTH = BitWidth.create(32);
 
-      if (data.updateClock(state.getPortValue(5), triggerType)) {
-         int a3 = (int)state.getPortValue(2).toLongValue();
-         Value wr3 = state.getPortValue(3);
-         Value r15 = state.getPortValue(4);
-         Value clr = state.getPortValue(6);
-         Value we3 = state.getPortValue(7);
+      if (data.updateClock(state.getPortValue(CLK), triggerType)) {
+         int a3 = (int)state.getPortValue(A3).toLongValue();
+         Value wr3 = state.getPortValue(WD3);
+         Value r15 = state.getPortValue(R15);
+         Value clr = state.getPortValue(CLR);
+         Value we3 = state.getPortValue(WE3);
 
          if (clr == Value.TRUE) {
             // System.out.println("CLR");
@@ -78,86 +92,65 @@ class RegisterFile16 extends InstanceFactory {
          System.out.println("R"+i+" = "+data.regs[i]);
       } */
 
-      int a1 = (int)state.getPortValue(0).toLongValue();
-      int a2 = (int)state.getPortValue(1).toLongValue();
+      int a1 = (int)state.getPortValue(A1).toLongValue();
+      int a2 = (int)state.getPortValue(A2).toLongValue();
       Value rd1 = a1 < 0 ?  Value.createUnknown(WIDTH) : data.regs[a1];
       Value rd2 = a2 < 0 ?  Value.createUnknown(WIDTH) : data.regs[a2];
-      state.setPort(8, rd1, 9);
-      state.setPort(9, rd2, 9); 
+      state.setPort(RD1, rd1, 9);
+      state.setPort(RD2, rd2, 9); 
    }
 
    @Override
    public void paintInstance(InstancePainter painter) {
       painter.drawRectangle(painter.getBounds(), "");
-      painter.drawPort(0, "A1", Direction.EAST);
-      painter.drawPort(1, "A2", Direction.EAST);
-      painter.drawPort(2, "A3", Direction.EAST);
-      painter.drawPort(3, "WD3", Direction.EAST);
-      painter.drawPort(4, "R15", Direction.EAST);
+      painter.drawPort(A1, "A1", Direction.EAST);
+      painter.drawPort(A2, "A2", Direction.EAST);
+      painter.drawPort(A3, "A3", Direction.EAST);
+      painter.drawPort(WD3, "WD3", Direction.EAST);
+      painter.drawPort(R15, "R15", Direction.EAST);
 
-      painter.drawClock(5, Direction.NORTH);
-      painter.drawPort(6, "CLR", Direction.NORTH);
-      painter.drawPort(7, "WE3", Direction.NORTH);
+      painter.drawClock(CLK, Direction.NORTH);
+      painter.drawPort(CLR, "CLR", Direction.NORTH);
+      painter.drawPort(WE3, "WE3", Direction.NORTH);
 
-      painter.drawPort(8, "RD1", Direction.WEST);
-      painter.drawPort(9, "RD2", Direction.WEST);
-      /*
+      painter.drawPort(RD1, "RD1", Direction.WEST);
+      painter.drawPort(RD2, "RD2", Direction.WEST);
+
       Graphics g = painter.getGraphics();
       Bounds bounds = painter.getBounds();
 
       Font font = g.getFont().deriveFont(9f);
 
-      // draw some pin labels
-      int left = bounds.getX();
-      int right = bounds.getX() + CHIP_WIDTH;
-      int top = bounds.getY();
-      int bottom =  bounds.getY() + CHIP_DEPTH;
-      GraphicsUtil.drawText(g, font, "W", left+2, top+CHIP_DEPTH/2-10,
-      GraphicsUtil.H_LEFT, GraphicsUtil.V_CENTER);
-      GraphicsUtil.drawText(g, font, "A", right-2, top+40,
-      GraphicsUtil.H_RIGHT, GraphicsUtil.V_CENTER);
-      GraphicsUtil.drawText(g, font, "B", right-2, bottom-40,
-      GraphicsUtil.H_RIGHT, GraphicsUtil.V_CENTER);
-      GraphicsUtil.drawText(g, "WE", left+CHIP_WIDTH/2-40, bottom-1,
-      GraphicsUtil.H_CENTER, GraphicsUtil.V_BOTTOM);
-      GraphicsUtil.drawText(g, "xW", left+CHIP_WIDTH/2-10, bottom-1,
-      GraphicsUtil.H_CENTER, GraphicsUtil.V_BOTTOM);
-      GraphicsUtil.drawText(g, "xA", left+CHIP_WIDTH/2+30, bottom-1,
-      GraphicsUtil.H_CENTER, GraphicsUtil.V_BOTTOM);
-      GraphicsUtil.drawText(g, "xB", left+CHIP_WIDTH/2+50, bottom-1,
-      GraphicsUtil.H_CENTER, GraphicsUtil.V_BOTTOM);
-
+      /*
       // draw some rectangles
       for (int i = 0; i < NUM_REGISTERS; i++) {
       drawBox(g, bounds, Color.GRAY, i);
       }
-
+      */
       // draw register labels
       for (int i = 0; i < NUM_REGISTERS; i++) {
-      GraphicsUtil.drawText(g, font, "x"+i,
-      bounds.getX() + boxX(i) - 1,
-      bounds.getY() + boxY(i) + (BOX_HEIGHT-1)/2,
-      GraphicsUtil.H_RIGHT, GraphicsUtil.V_CENTER);
+         GraphicsUtil.drawText(g, font, "x"+i,
+            bounds.getX() + 50,
+            bounds.getY() + 25 + i*10,
+            GraphicsUtil.H_RIGHT, GraphicsUtil.V_CENTER);
       }
-      
       if (!painter.getShowState()) {
-      return;
+         return;
       }
 
       // draw state
-      g.setColor(Color.LIGHT_GRAY);
-      g.fillRect(bounds.getX() + boxX(0)+1, bounds.getY() + boxY(0)+1, BOX_WIDTH-1, BOX_HEIGHT-1);
-      g.setColor(Color.BLACK); */
+      //g.setColor(Color.LIGHT_GRAY);
+      //g.fillRect(bounds.getX() + boxX(0)+1, bounds.getY() + boxY(0)+1, BOX_WIDTH-1, BOX_HEIGHT-1);
+      //g.setColor(Color.BLACK); 
       RegisterData data = RegisterData.get(painter, 16, 32);
-      /*
       for (int i = 0; i < NUM_REGISTERS; i++) {
          long v = data.regs[i].toLongValue();
-         String s = (data.regs[i].isFullyDefined() ? StringUtil.toHexString(WIDTH.getWidth(), v) : "?");
+         String s = (data.regs[i].isFullyDefined() ? StringUtil.toHexString(REGISTER_WIDTH, v) : "?");
          GraphicsUtil.drawText(g, font, s,
-         bounds.getX() + boxX(i) + BOX_WIDTH/2,
-         bounds.getY() + boxY(i) + (BOX_HEIGHT-1)/2,
+         bounds.getX() + 80,    
+         bounds.getY() + 25 + i*10,
          GraphicsUtil.H_CENTER, GraphicsUtil.V_CENTER);
-      } */
+      } 
    }
 
 }
